662b7e0554743d18ae4bec26bf3b3dab09a890d9
bomu <ruiqing.piao@thoughtworks.com>
2024-03-22 15:53:11 +0800

    fix style check

--- src/test/kotlin/com/thoughtworks/kotlin_basic/controller/ProductServiceTest.kt
+++ src/test/kotlin/com/thoughtworks/kotlin_basic/controller/ProductServiceTest.kt
@@ -1,4 +1,4 @@
-package com.thoughtworks.kotlin_basic.controller;
+package com.thoughtworks.kotlin_basic.controller
 
 import com.thoughtworks.kotlin_basic.MockData
 import com.thoughtworks.kotlin_basic.application.ProductService
@@ -22,32 +22,32 @@ class ProductServiceTest : MockData{
 
     @Test
     fun `should get product list`() {
-        val result = mockService.getProductList();
-        Assertions.assertEquals(5, result.size);
+        val result = mockService.getProductList()
+        Assertions.assertEquals(5, result.size)
     }
 
     @Test
     fun `should get inventory list`() {
-        val result = mockService.getInventoriesList();
-        Assertions.assertEquals(8, result.size);
+        val result = mockService.getInventoriesList()
+        Assertions.assertEquals(8, result.size)
     }
 
     @Test
     fun `price should not change when type is NORMAL`() {
         val dtos = ProductService().getProductDtoList(mockService.getProductList(), mockService.getInventoriesList())
         val dto = dtos.filter { it.sku == "ABC123" }.get(0)
-        Assertions.assertTrue(dto.type == "NORMAL");
-        Assertions.assertTrue(dto.quantity <= 30);
-        Assertions.assertEquals(dto.originalPrice, dto.showPrice);
+        Assertions.assertTrue(dto.type == "NORMAL")
+        Assertions.assertTrue(dto.quantity <= 30)
+        Assertions.assertEquals(dto.originalPrice, dto.showPrice)
     }
 
     @Test
     fun `price should change correctly when quantity less than or is 30`() {
         val dtos = ProductService().getProductDtoList(mockService.getProductList(), mockService.getInventoriesList())
         val dto = dtos.filter { it.sku == "JKL012" }.get(0)
-        Assertions.assertTrue(dto.type == "HIGH_DEMAND");
+        Assertions.assertTrue(dto.type == "HIGH_DEMAND")
         Assertions.assertTrue(dto.quantity <= 30)
-        Assertions.assertEquals(dto.originalPrice * 1.5f, dto.showPrice);
+        Assertions.assertEquals(dto.originalPrice * 1.5f, dto.showPrice)
     }
 
     @Test
@@ -57,6 +57,6 @@ class ProductServiceTest : MockData{
         val dto = dtos.filter { it.sku == "MNO345" }.get(0)
         Assertions.assertTrue(dto.type == "HIGH_DEMAND")
         Assertions.assertTrue(dto.quantity in 31..100)
-        Assertions.assertEquals(dto.originalPrice * 1.2f, dto.showPrice);
+        Assertions.assertEquals(dto.originalPrice * 1.2f, dto.showPrice)
     }
 }
